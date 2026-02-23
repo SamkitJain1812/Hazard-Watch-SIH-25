009284d574455a754b3c35ade4f725aa256dabfe
Samkit Jain <139643881+SamkitJain1812@users.noreply.github.com>
2026-02-23 13:19:23 +0530

    Delete SIH 2025 directory

--- SIH 2025/HazardWatch/migrations/1/down.sql
+++
@@ -1,7 +0,0 @@
-
-DROP INDEX idx_reports_status;
-DROP INDEX idx_reports_created_at;
-DROP INDEX idx_reports_hazard_type;
-DROP INDEX idx_reports_location;
-DROP INDEX idx_reports_user_id;
-DROP TABLE reports;
--- SIH 2025/HazardWatch/migrations/2/down.sql
+++
@@ -1,4 +0,0 @@
-
-DROP INDEX idx_user_roles_role;
-DROP INDEX idx_user_roles_user_id;
-DROP TABLE user_roles;
